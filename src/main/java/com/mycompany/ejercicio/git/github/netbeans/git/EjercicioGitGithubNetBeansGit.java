99bd35c4072bba3119e3ef8df31c7aac85b5dfb1
Agustin Bertero <agustinbertero.dev@gmail.com>
2024-03-25 16:58:32 +0100

    Inicializar repositorio - Primer Commit

---
+++ src/main/java/com/mycompany/ejercicio/git/github/netbeans/git/EjercicioGitGithubNetBeansGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio.git.github.netbeans.git;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class EjercicioGitGithubNetBeansGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
